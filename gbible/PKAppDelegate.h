949939800a4314f5ed42de6fc22f3bc0f4f28826
Kerri Shotts <kerrishotts@gmail.com>
2012-03-12 21:30:20 -0500

    Initial Commit

---
+++ gbible/PKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PKAppDelegate.h
+//  gbible
+//
+//  Created by Kerri Shotts on 3/12/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gbible/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gbible
+//
+//  Created by Kerri Shotts on 3/12/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PKAppDelegate class]));
+    }
+}
