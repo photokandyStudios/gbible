850e86ec781f9d84d649d67f56726e0bf3e715d4
Kerri Shotts <kerrishotts@gmail.com>
2012-03-30 00:39:57 -0500

    xcode stuff

---
+++ gbible/PKHighlights.h
@@ -0,0 +1,13 @@
+//
+//  PKHighlights.h
+//  gbible
+//
+//  Created by Kerri Shotts on 3/29/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PKHighlights : NSObject
+
+@end
---
+++ gbible/PKHighlights.m
@@ -0,0 +1,13 @@
+//
+//  PKHighlights.m
+//  gbible
+//
+//  Created by Kerri Shotts on 3/29/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import "PKHighlights.h"
+
+@implementation PKHighlights
+
+@end
