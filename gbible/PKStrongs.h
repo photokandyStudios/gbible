cc7af964027ed62207c0e47c564d4f25f2b600d5
Kerri Shotts <kerrishotts@gmail.com>
2012-04-04 01:08:09 -0500

    Xcode stuff

---
+++ gbible/PKStrongs.h
@@ -0,0 +1,13 @@
+//
+//  PKStrongs.h
+//  gbible
+//
+//  Created by Kerri Shotts on 4/3/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PKStrongs : NSObject
+
+@end
---
+++ gbible/PKStrongs.m
@@ -0,0 +1,13 @@
+//
+//  PKStrongs.m
+//  gbible
+//
+//  Created by Kerri Shotts on 4/3/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import "PKStrongs.h"
+
+@implementation PKStrongs
+
+@end
