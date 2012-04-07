4ce2ec2027fac9d831f7447878d72dc4e9a11532
Kerri Shotts <kerrishotts@gmail.com>
2012-04-07 17:56:43 -0500

    Xcode stuff; group reorg

---
+++ gbible/PKHistory.m
@@ -0,0 +1,13 @@
+//
+//  PKHistory.m
+//  gbible
+//
+//  Created by Kerri Shotts on 4/7/12.
+//  Copyright (c) 2012 photoKandy Studios LLC. All rights reserved.
+//
+
+#import "PKHistory.h"
+
+@implementation PKHistory
+
+@end
