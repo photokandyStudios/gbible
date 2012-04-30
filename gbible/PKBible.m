740ada3c9a250366e7ef536d774e1149ac6374c2
Kerri Shotts <kerrishotts@gmail.com>
2012-04-29 20:04:09 -0500

    New Search Implementation

--- gbible/PKBible.m
+++ gbible/PKBible.m
@@ -726,12 +726,77 @@
     {
         NSMutableArray *theMatches = [[NSMutableArray alloc] init];
         FMDatabase *db = [[PKDatabase instance] bible];
-        NSString *theLike = [NSString stringWithFormat:@"%%%@%%", [[theTerm lowercaseString] // RE: ISSUE #18
-                                           stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]];
-        FMResultSet *s = [db executeQuery:@"SELECT bibleBook, bibleChapter, bibleVerse FROM content WHERE bibleID in (?,?) AND TRIM(LOWER(bibleText)) LIKE ? ORDER BY 1,2,3",
+        
+        // in order to support Issue #32 (multiple words), we need to craft a SQL statement
+        // based on the inputs. There's really very little risk of SQL injection here --
+        // but it should be properly handled.
+        
+        // search support is as follows:
+        //   * "Jesus Mercy" results in an /Exact Phrase/ search. 
+        //   * Jesus Mercy results in an OR search: "Jesus" OR "Mercy"
+        //   * Jesus +Mercy results in an AND search: "Jesus" AND "Mercy"
+        //     * note: a prepended + should be removed
+        //   * Jesus -Mercy results in an NAND search: "Jesus" AND NOT "Mercy"
+        //   * Jesus%Mercy results in a wildcard search: Jesus, followed by Mercy in the verse.
+        //
+        // Words separated by SPACES will thus be transformed into LIKE "%word%". Any suspicious
+        // characters (", /, ;) will be removed prior. All words will be transformed to lowercase
+        // for proper searching.
+        
+        NSMutableString *searchPhrase = [[[theTerm lowercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] mutableCopy];
+        BOOL exactMatch = NO;
+        
+        if ([searchPhrase characterAtIndex:0] == '"' &&
+            [searchPhrase characterAtIndex:[searchPhrase length]-1] == '"')
+        {
+            exactMatch = YES;
+        }
+        
+        [searchPhrase replaceOccurrencesOfString:@"\"" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@";" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@"/" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@"\\" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];        
+        [searchPhrase replaceOccurrencesOfString:@"%" withString:@"%%" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        
+        if (!exactMatch)
+        {
+            NSArray *allTerms = [searchPhrase componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            searchPhrase = [@"" mutableCopy];
+            for (int i=0; i<[allTerms count]; i++)
+            {
+                NSMutableString *theWord = [[allTerms objectAtIndex:i] mutableCopy];
+                
+                switch ( [theWord characterAtIndex:0] )
+                {
+case '.':
+case '+':           [searchPhrase appendString: (i!=0 ? @"AND ( " : @"( ") ];
+                    [theWord deleteCharactersInRange:NSMakeRange(0, 1)];
+                    break;
+case '!':
+case '-':           [searchPhrase appendString: (i!=0 ? @"AND ( NOT " : @"( NOT ") ];
+                    [theWord deleteCharactersInRange:NSMakeRange(0, 1)];
+                    break;
+default:            [searchPhrase appendString: (i!=0 ? @"OR ( " : @"( ") ];
+                    break;
+                }
+                
+                [searchPhrase appendString:@"TRIM(LOWER(bibleText)) LIKE \"%"];
+                [searchPhrase appendFormat:theWord];
+                [searchPhrase appendString:@"%\" ) "];
+            }
+        }
+        else
+        {
+            NSString *theWord = searchPhrase;
+            searchPhrase = [@"" mutableCopy];
+            [searchPhrase appendString:@"( TRIM(LOWER(bibleText)) LIKE \"%"];
+            [searchPhrase appendFormat:theWord];
+            [searchPhrase appendString:@"%\" ) "];
+        }
+        //NSLog (@"Original: %@\nTransformed: %@", theTerm, searchPhrase);
+        FMResultSet *s = [db executeQuery:[NSString stringWithFormat: @"SELECT bibleBook, bibleChapter, bibleVerse FROM content WHERE bibleID in (?,?) AND (%@) ORDER BY 1,2,3", searchPhrase],
                                            [NSNumber numberWithInt:theGreekBible],
-                                           [NSNumber numberWithInt:theEnglishBible],
-                                           theLike];
+                                           [NSNumber numberWithInt:theEnglishBible]];
         while ([s next])
         {
             int theBook = [s intForColumnIndex:0];
--- gbible/PKStrongs.m
+++ gbible/PKStrongs.m
@@ -17,14 +17,17 @@
     {
         FMDatabase *db = [(PKDatabase *)[PKDatabase instance] bible];
         FMResultSet *s = [db executeQuery:@"SELECT * FROM strongs WHERE key = ?", theKey];
-        NSArray *theResult = nil;
+        NSMutableArray *theResult = [[NSMutableArray alloc]init ];
         if ([s next])
         {
-            theResult = [NSArray arrayWithObjects: [s stringForColumnIndex:0],
-                                                   [s stringForColumnIndex:1],
-                                                   [s stringForColumnIndex:2],
-                                                   [s stringForColumnIndex:3],
-                                                   [s stringForColumnIndex:4], nil];
+            for (int i=0; i<5; i++)
+            {
+                // Fixes issue #30
+                NSMutableString *theItem = [[s stringForColumnIndex:i] mutableCopy];
+                [theItem replaceOccurrencesOfString:@"[DQ]" withString:@"\"" options:0 range:NSMakeRange(0, [theItem length])];
+                [theResult addObject:theItem];
+            }
+            
         }
         return theResult;
     }
@@ -33,11 +36,77 @@
     {
         FMDatabase *db = [(PKDatabase *)[PKDatabase instance] bible];
 
+        // in order to support Issue #32 (multiple words), we need to craft a SQL statement
+        // based on the inputs. There's really very little risk of SQL injection here --
+        // but it should be properly handled.
+        
+        // search support is as follows:
+        //   * "Jesus Mercy" results in an /Exact Phrase/ search. 
+        //   * Jesus Mercy results in an OR search: "Jesus" OR "Mercy"
+        //   * Jesus +Mercy results in an AND search: "Jesus" AND "Mercy"
+        //     * note: a prepended + should be removed
+        //   * Jesus -Mercy results in an NAND search: "Jesus" AND NOT "Mercy"
+        //   * Jesus%Mercy results in a wildcard search: Jesus, followed by Mercy in the verse.
+        //
+        // Words separated by SPACES will thus be transformed into LIKE "%word%". Any suspicious
+        // characters (", /, ;) will be removed prior. All words will be transformed to lowercase
+        // for proper searching.
+        
+        NSMutableString *searchPhrase = [[[theTerm lowercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] mutableCopy];
+        BOOL exactMatch = NO;
+        
+        if ([searchPhrase characterAtIndex:0] == '"' &&
+            [searchPhrase characterAtIndex:[searchPhrase length]-1] == '"')
+        {
+            exactMatch = YES;
+        }
+        
+        [searchPhrase replaceOccurrencesOfString:@"\"" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@";" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@"/" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        [searchPhrase replaceOccurrencesOfString:@"\\" withString:@"" options:0 range:NSMakeRange(0, [searchPhrase length])];        
+        [searchPhrase replaceOccurrencesOfString:@"%" withString:@"%%" options:0 range:NSMakeRange(0, [searchPhrase length])];
+        
+        if (!exactMatch)
+        {
+            NSArray *allTerms = [searchPhrase componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            searchPhrase = [@"" mutableCopy];
+            for (int i=0; i<[allTerms count]; i++)
+            {
+                NSMutableString *theWord = [[allTerms objectAtIndex:i] mutableCopy];
+                
+                switch ( [theWord characterAtIndex:0] )
+                {
+case '.':
+case '+':           [searchPhrase appendString: (i!=0 ? @"AND ( " : @"( ") ];
+                    [theWord deleteCharactersInRange:NSMakeRange(0, 1)];
+                    break;
+case '!':
+case '-':           [searchPhrase appendString: (i!=0 ? @"AND ( NOT " : @"( NOT ") ];
+                    [theWord deleteCharactersInRange:NSMakeRange(0, 1)];
+                    break;
+default:            [searchPhrase appendString: (i!=0 ? @"OR ( " : @"( ") ];
+                    break;
+                }
+                
+                [searchPhrase appendString:@"TRIM(LOWER( key || ' ' || derivation || ' ' || lemma || ' ' || kjv_def || ' ' || strongs_def )) LIKE \"%"];
+                [searchPhrase appendFormat:theWord];
+                [searchPhrase appendString:@"%\" ) "];
+            }
+        }
+        else
+        {
+            NSString *theWord = searchPhrase;
+            searchPhrase = [@"" mutableCopy];
+            [searchPhrase appendString:@"( TRIM(LOWER(key || ' ' || derivation || ' ' || lemma || ' ' || kjv_def || ' ' || strongs_def)) LIKE \"%"];
+            [searchPhrase appendFormat:theWord];
+            [searchPhrase appendString:@"%\" ) "];
+        }
+
+        //NSLog (@"SQL: %@", searchPhrase);
         NSString *theNewTerm = [NSString stringWithFormat:@"%%%@%%", [[theTerm lowercaseString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]];
 
-        FMResultSet *s = [db executeQuery:@"SELECT * FROM strongs WHERE LOWER(key) LIKE ? OR LOWER(derivation) LIKE ? \
-                                                                     OR LOWER(lemma) LIKE ? OR LOWER(kjv_def) LIKE ? \
-                                                                     OR LOWER(strongs_def) LIKE ? ORDER BY 1 LIMIT 100", theNewTerm, theNewTerm, theNewTerm, theNewTerm, theNewTerm];
+        FMResultSet *s = [db executeQuery: [NSString stringWithFormat:@"SELECT * FROM strongs WHERE %@ ORDER BY 1", searchPhrase], theNewTerm];
         NSMutableArray *theResult = [[NSMutableArray alloc] init ];
         while ([s next]) {
             [theResult addObject:[s stringForColumnIndex:0]];
