c4802bd98741b6a5fa44188e548c3f0fd92e4620
Kerri Shotts <kerrishotts@gmail.com>
2012-12-14 02:48:26 -0600

    ios hierarchy viewr

---
+++ iOSHierarchyViewer.h
@@ -0,0 +1,15 @@
+//
+//  iOSHierarchyViewer.m
+//
+//  Copyright (c) 2012 Damian Kolakowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSHierarchyViewer : NSObject
+
++ (BOOL)start;
+
++ (void)stop;
+
+@end
