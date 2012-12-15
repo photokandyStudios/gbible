9ab7f9dbe746bd74bd166014efea624ffab07456
Kerri Shotts <kerrishotts@gmail.com>
2012-12-15 03:14:49 -0600

    Beginning work on a layout ui

--- gbible/PKLayoutController.m
+++ gbible/PKLayoutController.m
@@ -9,10 +9,37 @@
 #import "PKLayoutController.h"
 
 @interface PKLayoutController ()
+  @property (strong, nonatomic) UILabel * previewLabel;
+  @property (strong, nonatomic) UILabel * fontSizeLabel;
+  @property (strong, nonatomic) UILabel * greekFontLabel;
+  @property (strong, nonatomic) UILabel * englishFontLabel;
+  @property (strong, nonatomic) UILabel * rowSpacingLabel;
+  @property (strong, nonatomic) UIStepper * fontStepper;
+  @property (strong, nonatomic) UISlider * brightnessSlider;
+  @property (strong, nonatomic) UISegmentedControl * rowSpacingSelector;
+  @property (strong, nonatomic) UISegmentedControl * lineSpacingSelector;
+  @property (strong, nonatomic) UISegmentedControl * columnSelector;
+  @property (strong, nonatomic) UIPickerView * fontPicker;
+  @property (strong, nonatomic) UIImageView * decreaseBrightnessImage;
+  @property (strong, nonatomic) UIImageView * increaseBrightnessImage;
 
 @end
 
 @implementation PKLayoutController
+
+  @synthesize previewLabel;
+  @synthesize fontSizeLabel;
+  @synthesize greekFontLabel;
+  @synthesize englishFontLabel;
+  @synthesize rowSpacingLabel;
+  @synthesize fontStepper;
+  @synthesize brightnessSlider;
+  @synthesize rowSpacingSelector;
+  @synthesize lineSpacingSelector;
+  @synthesize columnSelector;
+  @synthesize fontPicker;
+  @synthesize decreaseBrightnessImage;
+  @synthesize increaseBrightnessImage;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,6 +54,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
 }
 
 - (void)didReceiveMemoryWarning
