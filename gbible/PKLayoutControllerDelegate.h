570ba9beda18fa118f568405393c7113d4d0ae63
Kerri Shotts <kerrishotts@gmail.com>
2013-01-04 01:56:46 -0600

    Layout Delegete

---
+++ gbible/PKLayoutControllerDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PKLayoutControllerDelegate.h
+//  gbible
+//
+//  Created by Kerri Shotts on 1/3/13.
+//  Copyright (c) 2013 photoKandy Studios LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class PKLayoutController;
+
+@protocol PKLayoutControllerDelegate <NSObject>
+
+  @required
+  -(void) didChangeLayout: (PKLayoutController *)sender;
+
+@end
