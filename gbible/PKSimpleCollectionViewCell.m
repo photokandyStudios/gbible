7ea85155d0b310a9c2e5333208e349dbb6cda730
Kerri Shotts <kerrishotts@gmail.com>
2013-02-07 23:36:36 -0600

    stop drawing rectangles

--- gbible/PKSimpleCollectionViewCell.m
+++ gbible/PKSimpleCollectionViewCell.m
@@ -35,7 +35,7 @@
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+- (void)dodrawRect:(CGRect)rect
 {
   CGContextRef context = UIGraphicsGetCurrentContext();
   CGContextSaveGState(context);
