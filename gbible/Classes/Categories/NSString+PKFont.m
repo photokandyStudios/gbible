bb3bb08388b0985ad9a12a57399f292e13fd5f88
Kerri Shotts <kerrishotts@gmail.com>
2013-09-13 01:33:42 -0500

    Make sure sizes/rects are integral; do nothing if no font/color sent

--- gbible/Classes/Categories/NSString+PKFont.m
+++ gbible/Classes/Categories/NSString+PKFont.m
@@ -11,21 +11,29 @@
 @implementation NSString (PKFont)
 - (void) drawInRect:(CGRect)rect withFont:(UIFont *)font withColor:(UIColor *)color usingLigatures:(BOOL)ligatures
 {
+  if (!font)  return;
+  if (!color) return;
+  
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
   {
     NSDictionary *attrs = @{ NSFontAttributeName: font,
                              NSForegroundColorAttributeName: color,
                              NSLigatureAttributeName: @(ligatures?1:0) };
-    [self drawInRect:rect withAttributes:attrs];
+    [self drawInRect:CGRectIntegral(rect) withAttributes:attrs];
   }
   else
   {
     [color setFill];
-    [self drawInRect:rect withFont:font];
+    [self drawInRect:CGRectIntegral(rect) withFont:font];
   }
 }
-- (void) drawAtPoint:(CGPoint)point withFont:(UIFont *)font withColor:(UIColor *)color usingLigatures:(BOOL) ligatures
+- (void) drawAtPoint:(CGPoint)aPoint withFont:(UIFont *)font withColor:(UIColor *)color usingLigatures:(BOOL) ligatures
 {
+  CGPoint point;
+  if (!font)  return;
+  if (!color) return;
+
+  point = CGPointMake(ceil(aPoint.x), ceil(aPoint.y));
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
   {
     NSDictionary *attrs = @{ NSFontAttributeName: font,
@@ -42,48 +50,63 @@
 
 - (CGSize) sizeWithFont: (UIFont *)font usingLigatures:(BOOL)ligatures
 {
+  CGSize size = CGSizeZero;
+  if (!font)  return size;
+  
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
   {
     NSDictionary *attrs = @{ NSFontAttributeName: font,
                              NSLigatureAttributeName: @(ligatures?1:0) };
-    return [self sizeWithAttributes:attrs];
+    size = [self sizeWithAttributes:attrs];
   }
   else
   {
-    return [self sizeWithFont:font];
+    size = [self sizeWithFont:font];
   }
-  return CGSizeZero;
+  size.height = ceil(size.height);
+  size.width = ceil(size.width);
+  return size;
 }
 
 - (CGSize) sizeWithFont: (UIFont *)font constrainedToSize: (CGSize)size usingLigatures:(BOOL)ligatures
 {
+  CGSize aSize = CGSizeZero;
+  if (!font)  return aSize;
+
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
   {
     NSDictionary *attrs = @{ NSFontAttributeName: font,
                              NSLigatureAttributeName: @(ligatures?1:0) };
-    return [self boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+    aSize= [self boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
   }
   else
   {
-    return [self sizeWithFont:font constrainedToSize:size];
+    aSize= [self sizeWithFont:font constrainedToSize:size];
   }
-  return CGSizeZero;
+  aSize.height = ceil(aSize.height);
+  aSize.width = ceil(aSize.width);
+  return aSize;
 }
 
 - (CGSize) sizeWithFont: (UIFont *)font constrainedToSize: (CGSize)size lineBreakMode:(NSLineBreakMode)lineBreakMode usingLigatures:(BOOL)ligatures
 {
+  CGSize aSize = CGSizeZero;
+  if (!font)  return aSize;
+
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
   {
     NSDictionary *attrs = @{ NSFontAttributeName: font,
                              NSLigatureAttributeName: @(ligatures?1:0) };
-    return [self boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+    aSize= [self boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
     
   }
   else
   {
-    return [self sizeWithFont:font constrainedToSize:size lineBreakMode:lineBreakMode];
+    aSize= [self sizeWithFont:font constrainedToSize:size lineBreakMode:lineBreakMode];
   }
-  return CGSizeZero;
+  aSize.height = ceil(aSize.height);
+  aSize.width = ceil(aSize.width);
+  return aSize;
 }
 
 @end
